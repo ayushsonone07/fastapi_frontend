6001b1bccc7ebc29f0207fbea113c3564a9f71d6
ayushsonone07 <90081700+ayushsonone07@users.noreply.github.com>
2026-03-26 04:00:17 +0000

    fixed DB

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,11 +1,23 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite:///./social.db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    pool_pre_ping=True,
+    pool_size=5,
+    max_overflow=10
 )
 
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
